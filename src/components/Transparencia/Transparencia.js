35d0a8bc43e3b422b65cab3f64ab97181890077c
Liz Durán <lizduran@Lizs-MacBook-Air.local>
2025-07-29 23:25:27 -0600

    transparency reports updated

--- src/components/Transparencia/Transparencia.js
+++ src/components/Transparencia/Transparencia.js
@@ -5,11 +5,12 @@ import { Link } from 'react-router-dom';
 const reportes = [
   { id: 1, nombre: "Reporte Anual 2023 ", enlace: "https://docs.google.com/spreadsheets/d/1XvXDg-pFXg1QulzYTvsLwUKILRdR1rK6KkBJFdPn8u8/edit?usp=sharing" },
   { id: 2, nombre: "Reporte Evento 2023", enlace: "https://docs.google.com/spreadsheets/d/1dwU5ZkSHSKdegO4DWGsRz0XWcYsqBN3hX1V-vhhTkcY/edit?usp=sharing" },
-  { id: 3, nombre: "Reporte Q1 2024", enlace: "https://app.charmverse.io/ethereum-mxico/ethereum-m-xico-reporte-q1-38607541387827093" },
-  { id: 4, nombre: "Reporte Q2 2024", enlace: "https://app.charmverse.io/ethereum-mxico/ethereum-m-xico-reporte-q2-571087265739902" },
+  { id: 3, nombre: "Reporte Q1 2024", enlace: "https://www.notion.so/Ethereum-M-xico-Reporte-Q1-2024-21b6a580104b8083b886d7975945c110" },
+  { id: 4, nombre: "Reporte Q2 2024", enlace: "https://www.notion.so/Ethereum-M-xico-Reporte-Q2-2024-21b6a580104b80f6b43dc56b68d67f61" },
   { id: 5, nombre: "Reporte Q3 2024 | Evento", enlace: "https://drive.google.com/file/d/1aLiOA9nQPm3naVHFmavpQNyEWI_GuDWp/view" },
-  { id: 6, nombre: "Reporte Q4 2024 ", enlace: "https://app.charmverse.io/ethereum-mxico/ethereum-m-xico-reporte-q4-39865205109212476" },
+  { id: 6, nombre: "Reporte Q4 2024 ", enlace: "https://www.notion.so/Ethereum-M-xico-Reporte-Q4-2024-21b6a580104b80d99a35c6b3530c866c" },
   { id: 7, nombre: "Reporte Q1 2025 ", enlace: "https://docs.google.com/document/d/1s_jkJM2gs2iyCUkt06qM1ZdV-wSvHuSQbettbRgTB5M/edit?tab=t.0" },
+  { id: 7, nombre: "Reporte Q1 2025 ", enlace: "https://docs.google.com/document/d/1XinwnHAuLDz4C_xGHBRPvGLw-1oEH3fLUGi3DX7V_cM/edit?usp=sharing" },
   { id: 8, nombre: "Comparte tu Feedback", enlace: "/feedback" },
    
 ];
