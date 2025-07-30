423b89ff2d967249556b5b2b8d1fb228bf6b83ca
Liz Durán <lizduran@Lizs-MacBook-Air.local>
2025-07-30 10:52:54 -0600

    links venues added

--- src/components/Venue/Venue.js
+++ src/components/Venue/Venue.js
@@ -10,6 +10,15 @@ function Venue() {
         <p className="text-event">
         Este año, ETH México 2025  transforma el centro histórico de Monterrey en un <b>Pop-Up Innovation Hub</b>, una red de actividades descentralizadas que durante tres días conectará tecnología, cultura y colaboración.
         </p>
+        <a 
+          href="https://maps.app.goo.gl/LQVppNcLc4EUCrBq6" 
+          className="text-event" 
+          style={{textDecoration: "none"}}
+          target="_blank"
+          rel="noopener noreferrer"
+        > 
+          Lista de Venues en Google Maps
+        </a>
       </div>
       
       <div className="mapa-venue">
